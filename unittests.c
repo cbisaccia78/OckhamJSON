cfb0e1191c9433e8184015844e69d3481c24da0f
cbisaccia78 <FEFQ9F1qaz0>
2023-03-14 16:58:00 -0400

    refactoring unit tests

--- unittests.c
+++ unittests.c
@@ -3,32 +3,15 @@
 #include "JSON.h"
 #include "StringUtils.h"
 
-#define LINELIMIT 4096
-
 /* Test that one equals one */
 static void test_simple_pass1(void) {
     CU_ASSERT_FATAL(1 == 1);
 }
+
+static void testStripWSUntilControlCharacter(void){
+    char *jsonText = "  \t\n  hello\n";
+    CU_ASSERT_FATAL(stripWSUntilControlCharacter(jsonText, QUOTATIONMARK) == 6)
+}
+
 CUNIT_CI_RUN(CU_MAIN_EXE_NAME,
              CUNIT_CI_TEST(test_simple_pass1));
-
-int main(int argc, char **argv){
-    /*
-        int c, i = 0;
-        char jsonText[LINELIMIT];
-        
-        getAll(jsonText, LINELIMIT);
-
-        DeSerializationTemplate *t;
-        Storage *s;
-        
-        deserializeFromTemplate(jsonText, t, s);
-    */
-
-    char *jsonText = "  \t\n  hello\n";
-    //getLine(jsonText, LINELIMIT);
-    printf("Echo: %s\n", jsonText);
-    int wsStripped = stripWSUntilControlCharacter(jsonText, QUOTATIONMARK);
-    printf("Number of white space parsed: %d\n", wsStripped);
-    printf("Result: %s\n", jsonText + wsStripped);
-}
