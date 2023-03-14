d02a64a8452268118b69aa36b3cfe062208d5411
cbisaccia78 <FEFQ9F1qaz0>
2023-03-14 16:33:26 -0400

    Prepping for integration with CUnit

--- unittests.c
+++ unittests.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include "External/CUnit/CUnitCI.h"
 #include "JSON.h"
 #include "StringUtils.h"
 
 #define LINELIMIT 4096
+
+/* Test that one equals one */
+static void test_simple_pass1(void) {
+    CU_ASSERT_FATAL(1 == 1);
+}
+CUNIT_CI_RUN(CU_MAIN_EXE_NAME,
+             CUNIT_CI_TEST(test_simple_pass1));
 
 int main(int argc, char **argv){
     /*
