1aab3d3eaf4499f9c96b8043efa0445db59d4a8f
cbisaccia78 <FEFQ9F1qaz0>
2023-03-20 14:32:16 -0400

    updated CMakeLists.txt and gitignore

--- unittests.c
+++ unittests.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-#include "CUnit/CUnitCI.h"
 #include "JSON.h"
 #include "StringUtils.h"
 
-/* Test that one equals one */
-static void test_simple_pass1(void) {
-    CU_ASSERT_FATAL(1 == 1);
+
+
+static void testStripWSUntilControlCharacter(void){  
+  char *jsonText = "  \t\n  hello\n";
+  CU_ASSERT_FATAL(stripWSUntilControlCharacter(jsonText, QUOTATIONMARK) == 6)
 }
 
-static void testStripWSUntilControlCharacter(void){
-    char *jsonText = "  \t\n  hello\n";
-    CU_ASSERT_FATAL(stripWSUntilControlCharacter(jsonText, QUOTATIONMARK) == 6)
-}
 
-CUNIT_CI_RUN(CU_MAIN_EXE_NAME,
-             CUNIT_CI_TEST(test_simple_pass1));
+int main(int argc, char**argv){
+  printf("Version Major: %s\nVersion Minor: %s\n", OckhamJSON_VERSION_MAJOR, OckhamJSON_VERSION_MINOR );
+}
