2b62cca98d0b165406c08150f9698e3349f501f1
Ghost <dancing.ghosts1@gmail.com>
2023-04-04 20:08:28 +0000

    Tests for string equality

--- StringUtilities/Tests/Equals.c
+++ StringUtilities/Tests/Equals.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "../StringUtils.h"
+#include "TestUtils.h"
+
+void initTestData(CaseList *data){
+    data = malloc(sizeof(CaseList)*2);
+    data->numCases = 2;
+    data->cases
+}
 
 int Equals(int argc, int **argv){
-    char *hello = "hello";
-    char *hello2 = "hello";
-    return !equals(hello, hello2);
+    CaseList *testData;
+    initTestData(testData);
+    int passed = 0;
+    for(int i = 0; i < testData->numCases; i++){
+        char **dataTuple = testData->cases[i].data;
+        passed = passed && !equals(dataTuple[0], dataTuple[1]);
+    }
+    
+    return passed;
 }
