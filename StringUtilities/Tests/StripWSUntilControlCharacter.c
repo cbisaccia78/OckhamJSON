a191a3b8605293ff12fed102fafb93bc33d7e37f
cbisaccia78 <FEFQ9F1qaz0>
2023-04-10 15:21:00 -0400

    fixed bug in WS strip

--- StringUtilities/Tests/StripWSUntilControlCharacter.c
+++ StringUtilities/Tests/StripWSUntilControlCharacter.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include "../StringUtils.h"
 
-int simpleTest(){
+int simpleWSTest(){
     char *test = "  \n\t{hello}";
     int numStripped = stripWSUntilControlCharacter(test, '{');
     return !(numStripped == 4 && equals(test + 4, "{hello}"));
 }
 
-int errorTest1(){
-    char *test = "  \n\t{hello}";
+int errorWSTest1(){
+    char *test = "  \n\thello}";
     int numStripped = stripWSUntilControlCharacter(test, '{');
     return !(numStripped == -1);
 }
 
-int errorTest2(){
+int errorWSTest2(){
     char *test = "  R\n\t{hello}";
     int numStripped = stripWSUntilControlCharacter(test, '{');
     return !(numStripped == -1);
 }
 
 int StripWSUntilControlCharacter(int argc, int **argv){
-    return simpleTest() || errorTest1() || errorTest2();
+    return simpleWSTest() || errorWSTest1() || errorWSTest2();
 }
