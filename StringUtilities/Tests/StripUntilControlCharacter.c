95d8fbd52285e2ffa875a9a0d07537608d3fd5a6
cbisaccia78 <FEFQ9F1qaz0>
2023-04-10 10:41:37 -0400

    string strip tests

--- StringUtilities/Tests/StripUntilControlCharacter.c
+++ StringUtilities/Tests/StripUntilControlCharacter.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include "../StringUtils.h"
 
-int StripUntilControlCharacter(int argc, char **argv){
+int simpleTest(){
     char *test = "  \n\t{hello}";
     int numStripped = stripUntilControlCharacter(test, '{');
     return !(numStripped == 4 && equals(test + 4, "{hello}"));
+}
+
+int errorTest(){
+    char *test = "  \n\thello}";
+    int numStripped = stripUntilControlCharacter(test, '{');
+    return !(numStripped == -1);
+}
+
+int StripUntilControlCharacter(int argc, char **argv){
+    return simpleTest() || errorTest();
 }
--- StringUtilities/Tests/StripWSUntilControlCharacter.c
+++ StringUtilities/Tests/StripWSUntilControlCharacter.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include "../StringUtils.h"
 
+int simpleTest(){
+    char *test = "  \n\t{hello}";
+    int numStripped = stripWSUntilControlCharacter(test, '{');
+    return !(numStripped == 4 && equals(test + 4, "{hello}"));
+}
 
-static int testStripWSUntilControlCharacter(void){  
-  char *jsonText = "  \t\n  \"hello\"\n";
-  return stripWSUntilControlCharacter(jsonText, '"');
+int errorTest1(){
+    char *test = "  \n\t{hello}";
+    int numStripped = stripWSUntilControlCharacter(test, '{');
+    return !(numStripped == -1);
+}
+
+int errorTest2(){
+    char *test = "  R\n\t{hello}";
+    int numStripped = stripWSUntilControlCharacter(test, '{');
+    return !(numStripped == -1);
 }
 
 int StripWSUntilControlCharacter(int argc, int **argv){
-    return !(testStripWSUntilControlCharacter() == 6);
+    return simpleTest() || errorTest1() || errorTest2();
 }
