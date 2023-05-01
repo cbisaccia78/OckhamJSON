3e5d1323dcb8235d9bbf6a1a2a9ecfeef88b355f
cbisaccia78 <FEFQ9F1qaz0>
2023-05-01 16:56:18 -0400

    working on include directory issues

--- Tests/ParseNumber.c
+++ Tests/ParseNumber.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "../../../JSON.h" //this is machine dependent, need a better solution
+#include "JSON.h" //this is machine dependent, need a better solution
 
 int ParseNumber(int argc, char **argv){
     char *intNum = "1234";
@@ -7,7 +7,7 @@ int ParseNumber(int argc, char **argv){
     char *floatNum = "1234.56";
     char *negativeFloatNum = "-1234.56";
     char *floatNumWithE = "1234.56e24";
-    char *floatNumWithE+ = "1234.56e+24";
-    char *floatNumWithE- = "1234.56e-24";
-    return !(parseNumber(&intNum, 'i') == 4)
+    char *floatNumWithEPlus = "1234.56e+24";
+    char *floatNumWithEMinus = "1234.56e-24";
+    return !(parseNumber(&intNum, 'i') == 4);
 }
